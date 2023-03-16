0e690f6beb7218a7a486443566e6a001bb8f7586
Kitenge Mukaba Jean-louis <101065560+Mukaba@users.noreply.github.com>
2023-03-16 10:07:18 +0000

    Add script to menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,11 @@ const closeBtn = document.getElementById('closeMenu');
 const menu = document.getElementById('menu');
 const noscroll = document.querySelector('html, body');
 
+menuBtn.addEventListener('click', () => {
+  menu.classList.add('active');
+  noscroll.classList.add('noscroll');
+});
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('active');
+  noscroll.classList.remove('noscroll');
+});
